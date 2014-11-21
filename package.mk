4edf3136330a6643993bd9ce583a70a74e31b0fc
Petr Gotthard <petr.gotthard@honeywell.com>
2014-11-21 13:29:03 -0800

    Improve build automation. Avoid dependence on rebar.

--- package.mk
+++ package.mk
@@ -2,16 +2,17 @@ RELEASABLE=true
 APP_NAME:=gen_smtp
 DEPS:=eiconv
 
-UPSTREAM_GIT:=https://github.com/gotthardp/gen_smtp.git
+UPSTREAM_GIT:=https://github.com/Vagabond/gen_smtp.git
 UPSTREAM_REVISION:=8f7ba01172e679e8b4262334cefc3451b4c36777
 RETAIN_ORIGINAL_VERSION:=true
 
 ORIGINAL_APP_FILE:=$(CLONE_DIR)/src/gen_smtp.app.src
 DO_NOT_GENERATE_APP_FILE=true
 
+# this is an auto-generated file
+EBIN_BEAMS+=$(EBIN_DIR)/smtp_rfc822_parse.beam
+
 define package_rules
-
-$(CLONE_DIR)/src/$(APP_NAME).app.src: $(CLONE_DIR)/.done
-	sed -i 's/git/"0.4.0"/' $(CLONE_DIR)/src/$(APP_NAME).app.src
-
+%.erl : %.yrl
+	$(ERLC) -o $$(@D) $$<
 endef
