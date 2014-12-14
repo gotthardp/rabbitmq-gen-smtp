c8a89df45a962ada18d2cef1675203f6543a01bf
Petr Gotthard <petr.gotthard@honeywell.com>
2014-12-14 13:38:07 -0800

    Switch to another genrevision

--- package.mk
+++ package.mk
@@ -3,7 +3,7 @@ APP_NAME:=gen_smtp
 DEPS:=eiconv
 
 UPSTREAM_GIT:=https://github.com/gotthardp/gen_smtp.git
-UPSTREAM_REVISION:=01017bd2f10475ecadb4e0ee2db38cf642d25a37
+UPSTREAM_REVISION:=19b33d179b20eefb35fbbc1dd3435f94dc794dd5
 RETAIN_ORIGINAL_VERSION:=true
 
 ORIGINAL_APP_FILE:=$(CLONE_DIR)/src/gen_smtp.app.src
