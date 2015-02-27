61e19ec5e2fdbdbc2ed77075e2b29569c4a1c389
Petr Gotthard <petr.gotthard@honeywell.com>
2015-02-26 17:34:29 -0800

    Update the commit hash.

--- package.mk
+++ package.mk
@@ -2,7 +2,7 @@ RELEASABLE=true
 APP_NAME:=gen_smtp
 
 UPSTREAM_GIT:=https://github.com/gotthardp/gen_smtp.git
-UPSTREAM_REVISION:=19b33d179b20eefb35fbbc1dd3435f94dc794dd5
+UPSTREAM_REVISION:=a62c02edae8cd951ef9ad48ac1808880f8978a76
 RETAIN_ORIGINAL_VERSION:=true
 
 ORIGINAL_APP_FILE:=$(CLONE_DIR)/src/gen_smtp.app.src
